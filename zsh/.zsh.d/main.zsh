de38f09cd8745674d83d5cb9c53eaceece09138c
Marcus Vinícius Belfort <marvinbelfort@gmail.com>
2018-01-02 23:50:52 -0200

    Reorganizing after idiot moves

--- zsh/.zsh.d/main.zsh
+++ zsh/.zsh.d/main.zsh
@@ -17,12 +17,17 @@ source ~/.zsh.d/plugins.zsh
 source ~/.zsh.d/nin-vi-mode.zsh
 source ~/.zsh.d/fzf.zsh
 
+for file in $(find ~/.zsh.d | grep plugin.zsh)
+do
+  source $file
+done
+
 #TODO: corrigir
-source /home/marvin/Downloads/dotfiles/zsh/plugins/docker-zsh-completion/docker-zsh-completion.plugin.zsh
-source /home/marvin/Downloads/dotfiles/zsh/plugins/zsh-completions/zsh-completions.plugin.zsh
-source /home/marvin/Downloads/dotfiles/zsh/plugins/resty/resty.plugin.zsh
-source /home/marvin/Downloads/dotfiles/zsh/plugins/k/k.sh
-source /home/marvin/Downloads/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+# source /home/marvin/Downloads/dotfiles/zsh/plugins/docker-zsh-completion/docker-zsh-completion.plugin.zsh
+# source /home/marvin/Downloads/dotfiles/zsh/plugins/zsh-completions/zsh-completions.plugin.zsh
+# source /home/marvin/Downloads/dotfiles/zsh/plugins/resty/resty.plugin.zsh
+# source /home/marvin/Downloads/dotfiles/zsh/plugins/k/k.sh
+# source /home/marvin/Downloads/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 
 # init completion system
 source ~/.zsh.d/completions.zsh
