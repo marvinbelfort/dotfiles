b615acfc90929eb9d860c37fef6ac30a9fcc67d3
Marcus Vinícius Belfort <marvinbelfort@gmail.com>
2018-01-27 20:30:46 -0200

    Add dir_tracker

---
+++ zsh/.zsh.d/dir_tracker.zsh
@@ -0,0 +1,27 @@
+
+DIR_HISTORY="$HOME/.dir_history"
+
+pwd_hook() {
+  touch $DIR_HISTORY
+  local _cwd=$(pwd)
+  local num=22
+  lines=($_cwd)
+  for line in $(awk '!a[$0]++' $DIR_HISTORY)
+  do
+    if [[ $_cwd !=  $line ]]; then
+      lines+=($line)
+    fi
+  done
+  printf "%s\n" "${lines[@]:0:${num}}" > $DIR_HISTORY
+}
+
+show_and_choose() {
+  choice=$( tail -n +2 $DIR_HISTORY | cat - -b | fzf | awk '{print $2}')
+  if [ -n "$choice" ]; then
+    cd $choice
+  fi
+}
+
+# Hook functions
+chpwd_functions+=(pwd_hook)
+typeset -U chpwd_functions
